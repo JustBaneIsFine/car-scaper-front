b1ef15bec2fc92c86f4b7033a032832d14e37e53
Bane <bane.baki.1997@gmail.com>
2023-01-23 19:17:26 +0100

    Adding display style: table list

--- src/components/CarItemList.tsx
+++ src/components/CarItemList.tsx
@@ -1,16 +1,42 @@
-export default function CarItemList() {
+import { CarInterface } from '../interfaces/carInterface';
+
+export default function CarItemList({
+  carObject,
+  index,
+}: {
+  carObject: CarInterface;
+  index: number;
+}) {
   return (
-    <button
-      type="button"
-      onClick={() => {
-        fetch('http://localhost:3000/').then((x) => {
-          x.json().then((result) => {
-            console.log(result);
-          });
-        });
-      }}
+    <tr
+      className="text-center"
+      style={{ backgroundColor: index % 2 === 0 ? 'rgba(3,3,3,0.2)' : '' }}
     >
-      CLICK TO SEND REQUEST
-    </button>
+      <td className=" border border-slate-900 text-left dark:border-slate-400 sm:border-dashed">
+        <a
+          href={carObject.href}
+          className="decoration-slice underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {carObject.CarName}
+        </a>
+      </td>
+      <td className="border border-slate-900 dark:border-slate-400 sm:border-dashed">
+        {carObject.CarPrice}
+      </td>
+      <td className="border border-slate-900 dark:border-slate-400 sm:border-dashed">
+        {carObject.CarYear}
+      </td>
+      <td className="border border-slate-900 dark:border-slate-400 sm:border-dashed">
+        {carObject.CarCC}
+      </td>
+      <td className="border border-slate-900 dark:border-slate-400 sm:border-dashed">
+        {carObject.CarFuel}
+      </td>
+      <td className="border border-slate-900 dark:border-slate-400 sm:border-dashed">
+        {carObject.CarKM}
+      </td>
+    </tr>
   );
 }
--- src/containers/CarItemsTable.tsx
+++ src/containers/CarItemsTable.tsx
@@ -1,18 +1,34 @@
 import LoadingData from '../components/loadingData';
 import CarItemCard from '../components/CarItemCard';
+import { CarInterface } from '../interfaces/carInterface';
+import CarItemList from '../components/CarItemList';
 
-export default function CarItemsTable({
-  list,
-}: {
-  list: Array<string> | undefined;
-}) {
-  // if data is undefined, we set a loading screen, or nothing.
-
-  return list === undefined ? (
-    <LoadingData />
-  ) : (
-    list.forEach((element) => {
-      return <CarItemCard data={element} />;
-    })
+export default function CarItemsTable({ list }: { list: Array<CarInterface> }) {
+  return (
+    <div className=" text-xs font-semibold   sm:mx-2 sm:text-base sm:font-normal">
+      <table className="w-full table-auto border-collapse border border-slate-500">
+        <thead>
+          <tr>
+            <th>Model</th>
+            <th>Cena</th>
+            <th>Godiste</th>
+            <th>Kubikaza</th>
+            <th>Gorivo</th>
+            <th>Kilometraza</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((carObject, index) => {
+            return (
+              <CarItemList
+                carObject={carObject}
+                key={carObject.id}
+                index={index}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
