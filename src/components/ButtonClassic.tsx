b76eeb789a48cf222f95daf0f03028f6f62eac20
Bane <bane.baki.1997@gmail.com>
2023-01-27 18:56:19 +0100

    Added support for style prop, for button component

--- src/components/ButtonClassic.tsx
+++ src/components/ButtonClassic.tsx
@@ -1,15 +1,20 @@
 export default function ButtonClassic({
   name,
   onclick,
+  st,
 }: {
   name: string;
   onclick: any;
+  // eslint-disable-next-line react/require-default-props
+  st?: string;
 }) {
   return (
     <button
       onClick={onclick}
       type="button"
-      className="m-2 rounded-lg border-2 border-black border-opacity-60  pl-3 pr-3 pt-1 pb-1   text-slate-500  duration-300 ease-in-out hover:bg-slate-200 hover:text-slate-800 "
+      className={`${
+        st === undefined ? '' : st
+      } m-2 rounded-lg border-2 border-black border-opacity-60  pl-3 pr-3 pt-1 pb-1   text-slate-500  duration-300 ease-in-out hover:bg-slate-200 hover:text-slate-800`}
     >
       {name}
     </button>
