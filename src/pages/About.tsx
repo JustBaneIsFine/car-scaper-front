034325fb4155dc3a2ff0d292db78dcb01bdae3ee
Bane <bane.baki.1997@gmail.com>
2023-03-03 19:05:43 +0100

    Adding bugs and features to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,44 @@
+/* eslint-disable react/no-array-index-key */
+import list from '../assets/bugsNFeatures.json';
+
 export default function About({ x }: { x: string }) {
   return (
     <div className=" m-auto mt-5 flex w-3/4 flex-col text-center dark:text-slate-100 sm:mt-8">
       <div className="text-2xl sm:text-4xl">About the website</div>
-      <p className="mt-2 text-sm">
+      <p className="mt-2 mb-5 text-sm">
         Important: This website is only used for demonstration purposes for my
-        portfolio. If you are the owner of the below websites and do not want
-        the websites to be scraped please contact me!
+        portfolio. <br />
+        If you are the owner of the below websites and do not want the websites
+        to be scraped please contact me!
       </p>
+      <div className=" inline-flex  w-full justify-around">
+        <div className="w-1/4 ">
+          <span className="text-lg">Known bugs to be fixed:</span>
+          <div className="m-auto mt-5 w-full border border-dashed border-white  opacity-50" />
+          <ul className=" list-decimal text-left">
+            {list.bugs.length ? (
+              list.bugs.map((text, i) => {
+                return <li key={i}>{text}</li>;
+              })
+            ) : (
+              <li> There are no known bugs, awesome! </li>
+            )}
+          </ul>
+        </div>
+        <div>
+          <span className="text-lg">Features to be implemented:</span>
+          <div className="m-auto mt-5 w-full border border-dashed border-white  opacity-50" />
+          <ul className=" list-decimal text-left">
+            {list.features.length ? (
+              list.features.map((text, i) => {
+                return <li key={i}>{text}</li>;
+              })
+            ) : (
+              <li> There are no more features to add. </li>
+            )}
+          </ul>
+        </div>
+      </div>
       <div className="mt-10 flex-col sm:grid sm:grid-cols-3">
         <div>
           <div className="text-xl  sm:text-2xl">Websites used:</div>
@@ -44,10 +76,9 @@ export default function About({ x }: { x: string }) {
           </div>
           <div className="m-auto mt-5 w-1/2 border border-dashed border-white  opacity-50" />
           <p className="mt-3">Front-End: </p>
-          <p>Vite, Typescript, Tailwind, React</p>
-          <p>Eslint, prettier </p>
+          <p>Typescript, Tailwind, React, Vite</p>
           <p className="mt-3">Back End:</p>
-          <p>Express, Mongo, Node</p>
+          <p>Express, Mongo, Node, Puppeteer</p>
         </div>
 
         <div>
